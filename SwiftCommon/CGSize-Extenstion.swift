1727743e189db491ab87a878edd58b97fc7cc9a6
Andrew Bradnan <andbr@microsoft.com>
2016-05-24 10:22:36 -0700

    Initial commit

---
+++ SwiftCommon/CGSize-Extenstion.swift
@@ -0,0 +1,15 @@
+//
+//  CGSize-Extenstion.swift
+//  SwiftCommon
+//
+//  Created by Andrew Bradnan on 5/23/16.
+//  Copyright © 2016 Belkin. All rights reserved.
+//
+
+import Foundation
+
+extension CGSize {
+    func swap() -> CGSize {
+        return CGSizeMake(self.height, self.width)
+    }
+}
---
+++ SwiftCommon/Dispatch.swift
@@ -0,0 +1,18 @@
+/**
+ # Dispatch.swift
+ ##  Echo
+
+ - Author: Andrew Bradnan
+ - Date: 5/23/16
+ - Copyright: © 2016 Belkin International. All rights reserved.
+*/
+
+typealias Block = () -> Void
+
+func secondsFromNow(secs: Double) -> dispatch_time_t {
+    return dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), Int64(secs * Double(NSEC_PER_SEC)))
+}
+
+func dispatch_after(secs: Double, block: Block) {
+    dispatch_after(secondsFromNow(secs), dispatch_get_main_queue(), block)
+}
